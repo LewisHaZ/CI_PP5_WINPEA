64684f6b581250621f091d6c831f8d34c1ce8c21
LewisHaZ <feldman.lewis@Gmail.com>
2023-11-11 17:59:44 +0000

    Adjust URL in bag so user can remove items

--- winpea_e_commerce/settings.py
+++ winpea_e_commerce/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
-    'bag'
+    'bag',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +108,7 @@ WSGI_APPLICATION = 'winpea_e_commerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
