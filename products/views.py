978ed6af3fa03a9f0e7f168fa24fad971db6b507
LewisHaZ <feldman.lewis@Gmail.com>
2023-11-15 15:32:48 +0000

    Adjust bag view to include additional toasts for adding/removing products

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
