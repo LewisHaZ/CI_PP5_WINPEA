04cf707231f8d9ecdce65d8e65773fe2b0f6b1af
LewisHaZ <feldman.lewis@Gmail.com>
2023-11-15 15:18:54 +0000

    Added bootstrap toasts for users to recognise inputs

--- winpea_e_commerce/settings.py
+++ winpea_e_commerce/settings.py
@@ -79,6 +79,8 @@ TEMPLATES = [
     },
 ]
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = (
     # Needed to login by username in django admin, regardless of 'allauth'abs
     'django.contrib.auth.backends.ModelBackend',
