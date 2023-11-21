5928bda9318b1ead5ea152062b37600c5778bba6
LewisHaZ <feldman.lewis@Gmail.com>
2023-11-21 11:03:58 +0000

    Altered debug setting and removed secret key

--- winpea_e_commerce/settings.py
+++ winpea_e_commerce/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-on))gb-llphr@q9%j&bnof11wx(+l=xpj%%6l^@5ft=4_rul1v'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['pp5-winpea-c20cdf8bb1f3.herokuapp.com', 'localhost']
 
