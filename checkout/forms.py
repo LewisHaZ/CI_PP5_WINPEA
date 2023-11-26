fdef6800500fd646d326975f6cee626ec5657c6a
LewisHaZ <feldman.lewis@Gmail.com>
2023-11-26 15:23:51 +0000

    Adjust checkout parameters

--- checkout/forms.py
+++ checkout/forms.py
@@ -36,4 +36,6 @@ class OrderForm(forms.ModelForm):
                     placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].widget.attrs['aria-label'] = placeholders[
+                    field]
             self.fields[field].label = False
--- checkout/views.py
+++ checkout/views.py
@@ -3,17 +3,18 @@ from django.shortcuts import (
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
+import stripe
+import json
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
-
-from products.models import Product
+from products.models import Product, Category
+from bag.contexts import bag_contents
 from profiles.models import UserProfile
 from profiles.forms import UserProfileForm
-from bag.contexts import bag_contents
 
-import stripe
-import json
+
+
 
 
 @require_POST
